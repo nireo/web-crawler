3b3d4310485b30f4f7d4a6df15fc72ce545bed57
nireo <eemil.lehtikangas1@gmail.com>
2020-03-06 12:34:58 +0200

    added a item struct

--- main.go
+++ main.go
@@ -3,18 +3,46 @@ package main
 import (
 	"fmt"
 	"github.com/PuerkitoBio/goquery"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/sqlite"
 	"log"
 	"net/http"
+	"net/url"
 	"strings"
 )
+
+var db *gorm.DB
+
+type Item struct {
+	URL    string
+	Scheme string
+	Host   string
+	Path   string
+}
 
 func processElement(index int, element *goquery.Selection) {
 	href, exists := element.Attr("href")
 	if exists {
 		if strings.Contains(href, "https") {
-			fmt.Println(href)
+			parsed, err := url.Parse(href)
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			item := Item{
+				URL:    href,
+				Scheme: parsed.Scheme,
+				Host:   parsed.Host,
+				Path:   parsed.Path,
+			}
+
+			fmt.Println(item)
 		}
 	}
+}
+
+func initializeDB() {
+	db, _ = gorm.Open("./crawl.db", "sqlite")
 }
 
 func main() {
